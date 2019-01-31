53b92711cbbae87b71b9ee0f082ec5aa31b28a0c
Oleksiy Dubovyk <oleksiy_dubovyk@epam.com>
2019-02-01 00:51:46 +0200

    webpack, babel, typescript

---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const App = () => <div>React App</div>;
+
+render(<App/>, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
+};
